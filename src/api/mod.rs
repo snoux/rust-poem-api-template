2f6a6586d682b0ff31e5df66baf70d33cfc5a60a
snow <77766409+snow-xf@users.noreply.github.com>
2025-06-02 21:25:57 +0800

    调整可通过模板cargo generate创建项目

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -6,26 +6,35 @@ pub mod user;
 
 use poem_openapi::{OpenApiService, ContactObject, LicenseObject};
 
+const AUTHOR: &str = "{{ author }}";
+const GITHUB: &str = "{{ github }}";
+
 /// 创建OpenAPI服务
 /// 
 /// 聚合所有API模块，并配置OpenAPI文档
 pub fn create_api_service() -> OpenApiService<(user::UserController), ()> {
-    OpenApiService::new(
+    let mut service = OpenApiService::new(
         (
             user::UserController::default() // 用户管理API控制器
         ),
-        "API接口文档", // API标题
+        "{{ doc_title }}", // API文档标题
         env!("CARGO_PKG_VERSION"), // API版本（从Cargo.toml获取）
     )
     .server("/api") // API基础路径
-    .description("一个基于Rust和Poem框架的模块化后端API模板") // API描述
-    .contact(ContactObject::new()
-        .name("snow-xf")
-        .url("https://github.com/snow-xf/rust-poem-api-template")
-        // .email("livefei@live.com")
-    ) // 联系人信息
-    .license(LicenseObject::new("MIT")
-        .url("https://opensource.org/licenses/MIT")
-    ) // 许可证
-    // .external_document("https://github.com/snow-xf/rust-poem-api-template") // 外部文档
+    .description("{{ project_description }}") // API描述
+    .license(
+        LicenseObject::new("MIT")
+            .url("https://opensource.org/licenses/MIT"),
+    ); // 许可证
+
+    if !AUTHOR.trim().is_empty() && !GITHUB.trim().is_empty() {
+        service = service.contact(
+            ContactObject::new()
+                .name(AUTHOR)
+                .url(GITHUB),
+        );
+    }
+
+
+    service
 }
